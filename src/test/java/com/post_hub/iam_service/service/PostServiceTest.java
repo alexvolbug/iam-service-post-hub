8eb59bcdf7c5977a2a3c41a8a2761ef029a83836
alex20x20 <alex20x20@gmail.com>
2026-03-05 21:56:29 +0300

    Unit test by postsevice 2 tests to add

--- src/test/java/com/post_hub/iam_service/service/PostServiceTest.java
+++ src/test/java/com/post_hub/iam_service/service/PostServiceTest.java
@@ -4,16 +4,26 @@ import com.post_hub.iam_service.mapper.PostMapper;
 import com.post_hub.iam_service.model.dto.post.PostDTO;
 import com.post_hub.iam_service.model.entity.Post;
 import com.post_hub.iam_service.model.entity.User;
+import com.post_hub.iam_service.model.exception.NotFoundException;
 import com.post_hub.iam_service.repository.PostRepository;
 import com.post_hub.iam_service.repository.UserRepository;
 import com.post_hub.iam_service.service.impl.PostServiceImpl;
 import com.post_hub.iam_service.utils.ApiUtils;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.never;
 
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
@@ -55,4 +65,33 @@ public class PostServiceTest {
         testPostDTO.setTitle("Test Post");
         testPostDTO.setContent("Test Content");
     }
+    @Test
+    void getById_PostExists_ReturnsPostDTO() {
+        when(postRepository.findByIdAndDeletedFalse(1)).thenReturn(Optional.of(testPost));
+        when(postMapper.toPostDTO(testPost)).thenReturn(testPostDTO);
+
+        PostDTO result = postService.getById(1).getPayload();
+
+        assertNotNull(result);
+        assertEquals(testPostDTO.getId(), result.getId());
+        assertEquals(testPostDTO.getTitle(), result.getTitle());
+
+        verify(postRepository, times(1)).findByIdAndDeletedFalse(1);
+        verify(postMapper, times(1)).toPostDTO(testPost);
+    }
+
+    @Test
+    void getByID_PostNotFound_ThrowsException() {
+        when(postRepository.findByIdAndDeletedFalse(999)).thenReturn(Optional.empty());
+
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> postService.getById(999));
+
+        assertTrue(exception.getMessage().contains("not found"));
+
+        verify(postRepository, times(1)).findByIdAndDeletedFalse(999);
+        verify(postMapper, never()).toPostDTO(any(Post.class));
+    }
 }
+
+
+
