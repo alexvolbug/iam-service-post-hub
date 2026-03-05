ab86b20a383391932486f27e11c99e5cdb2b3966
alex20x20 <alex20x20@gmail.com>
2026-03-06 00:32:50 +0300

    Add test new test for commit

--- src/test/java/com/post_hub/iam_service/service/CommentServiceTest.java
+++ src/test/java/com/post_hub/iam_service/service/CommentServiceTest.java
@@ -5,17 +5,30 @@ import com.post_hub.iam_service.model.dto.comment.CommentDTO;
 import com.post_hub.iam_service.model.entity.Comment;
 import com.post_hub.iam_service.model.entity.Post;
 import com.post_hub.iam_service.model.entity.User;
+import com.post_hub.iam_service.model.exception.NotFoundException;
+import com.post_hub.iam_service.model.request.comment.CommentRequest;
 import com.post_hub.iam_service.repository.CommentRepository;
 import com.post_hub.iam_service.repository.PostRepository;
 import com.post_hub.iam_service.repository.UserRepository;
 import com.post_hub.iam_service.service.impl.CommentServiceImpl;
 import com.post_hub.iam_service.utils.ApiUtils;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
@@ -64,4 +77,55 @@ public class CommentServiceTest {
         testCommentDTO.setId(1);
         testCommentDTO.setMessage("Test Comment");
     }
+    @Test
+    void getCommentById_CommentExists_ReturnsCommentDTO() {
+        when(commentRepository.findByIdAndDeletedFalse(1)).thenReturn(Optional.of(tesComment));
+        when(commentMapper.toDto(tesComment)).thenReturn(testCommentDTO);
+
+        CommentDTO result = commentService.getCommentById(1).getPayload();
+
+        assertNotNull(result);
+        assertEquals(testCommentDTO.getId(), result.getId());
+        assertEquals(testCommentDTO.getMessage(), result.getMessage());
+
+        verify(commentRepository, times(1)).findByIdAndDeletedFalse(1);
+        verify(commentMapper, times(1)).toDto(tesComment);
+    }
+
+    @Test
+    void getCommentById_CommentNotFound_ThrowsException() {
+        when(commentRepository.findByIdAndDeletedFalse(999)).thenReturn(Optional.empty());
+
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> commentService.getCommentById(999));
+
+        assertTrue(exception.getMessage().contains("not found"));
+
+        verify(commentRepository, times(1)).findByIdAndDeletedFalse(999);
+        verify(commentMapper, never()).toDto(any(Comment.class));
+    }
+
+    @Test
+    void createComment_OK() {
+        CommentRequest request = new CommentRequest(1, "New comment");
+
+        when(apiUtils.getUserIdFromAuthentication()).thenReturn(testUser.getId());
+        when(userRepository.findById(testUser.getId())).thenReturn(Optional.of(testUser));
+        when(postRepository.findByIdAndDeletedFalse(testPost.getId())).thenReturn(Optional.of(testPost));
+        when(commentMapper.createComment(request, testUser, testPost)).thenReturn(tesComment);
+        when(commentRepository.save(any(Comment.class))).thenReturn(tesComment);
+        when(commentMapper.toDto(tesComment)).thenReturn(testCommentDTO);
+
+        CommentDTO result = commentService.createComment(request).getPayload();
+
+        assertNotNull(result);
+        assertEquals(testCommentDTO.getMessage(), result.getMessage());
+
+        verify(apiUtils, times(1)).getUserIdFromAuthentication();
+        verify(userRepository, times(1)).findById(testUser.getId());
+        verify(postRepository, times(1)).findByIdAndDeletedFalse(testPost.getId());
+        verify(commentRepository, times(1)).save(any(Comment.class));
+        verify(commentMapper, times(1)).toDto(any(Comment.class));
+    }
 }
+
+
