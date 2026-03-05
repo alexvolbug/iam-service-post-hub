97c59e0ebc28e1adc07a26ac1a09eb87c80561d9
alex20x20 <alex20x20@gmail.com>
2026-03-06 00:27:54 +0300

    Add test to create CommentServiceTest

---
+++ src/test/java/com/post_hub/iam_service/service/CommentServiceTest.java
@@ -0,0 +1,67 @@
+package com.post_hub.iam_service.service;
+
+import com.post_hub.iam_service.mapper.CommentMapper;
+import com.post_hub.iam_service.model.dto.comment.CommentDTO;
+import com.post_hub.iam_service.model.entity.Comment;
+import com.post_hub.iam_service.model.entity.Post;
+import com.post_hub.iam_service.model.entity.User;
+import com.post_hub.iam_service.repository.CommentRepository;
+import com.post_hub.iam_service.repository.PostRepository;
+import com.post_hub.iam_service.repository.UserRepository;
+import com.post_hub.iam_service.service.impl.CommentServiceImpl;
+import com.post_hub.iam_service.utils.ApiUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+public class CommentServiceTest {
+
+    @Mock
+    private CommentRepository commentRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PostRepository postRepository;
+
+    @Mock
+    private CommentMapper commentMapper;
+
+    @Mock
+    private ApiUtils apiUtils;
+
+    @InjectMocks
+    private CommentServiceImpl commentService;
+
+    private Comment tesComment;
+    private CommentDTO testCommentDTO;
+    private User testUser;
+    private Post testPost;
+
+    @BeforeEach
+    void setUp() {
+        testUser = new User();
+        testUser.setId(1);
+        testUser.setUsername("TestUser");
+
+        testPost = new Post();
+        testPost.setId(1);
+        testPost.setTitle("TestPost");
+
+        tesComment = new Comment();
+        tesComment.setId(1);
+        tesComment.setMessage("Test Comment");
+        tesComment.setPost(testPost);
+        tesComment.setUser(testUser);
+
+        testCommentDTO = new CommentDTO();
+        testCommentDTO.setId(1);
+        testCommentDTO.setMessage("Test Comment");
+    }
+}
