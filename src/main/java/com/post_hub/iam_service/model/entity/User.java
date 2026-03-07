a803af82e6b17d044b21b4a8ca2de5a2b32ad50c
alex20x20 <alex20x20@gmail.com>
2026-03-07 12:24:57 +0300

    user service test

--- src/main/java/com/post_hub/iam_service/model/entity/User.java
+++ src/main/java/com/post_hub/iam_service/model/entity/User.java
@@ -57,7 +57,7 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Post> posts;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
---
+++ src/test/java/com/post_hub/iam_service/integration/user_controller/UserControllerTest.java
@@ -0,0 +1,72 @@
+package com.post_hub.iam_service.integration.user_controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.post_hub.iam_service.IamServiceApplication;
+import com.post_hub.iam_service.model.entity.User;
+import com.post_hub.iam_service.model.exception.InvalidDataException;
+import com.post_hub.iam_service.repository.UserRepository;
+import com.post_hub.iam_service.security.JwtTokenProvider;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest(classes = IamServiceApplication.class)
+@AutoConfigureMockMvc
+@ExtendWith({MockitoExtension.class, SpringExtension.class})
+public class UserControllerTest {
+
+    @Autowired @Setter
+    private MockMvc mockMvc;
+
+    @Autowired @Setter
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired @Setter
+    private UserRepository userRepository;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private String adminJwt;
+    private String userJwt;
+
+    @BeforeAll
+    @Transactional
+    void authorize() {
+        // Admin authorization (ID: 1)
+        User admin = userRepository.findById(1)
+                .orElseThrow(() -> new InvalidDataException("Admin with ID: 1 not found"));
+        Hibernate.initialize(admin.getRoles());
+        this.adminJwt = "Bearer " + jwtTokenProvider.generateToken(admin);
+
+        // Regular user authorization (ID: 3)
+        User user = userRepository.findById(3)
+                .orElseThrow(() -> new InvalidDataException("User with ID: 3 not found"));
+        Hibernate.initialize(user.getRoles());
+        this.userJwt = jwtTokenProvider.generateToken(user);
+    }
+
+    @Test
+    void getAllUsers_200_OK() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/users/all")
+                        .header(HttpHeaders.AUTHORIZATION, adminJwt)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
