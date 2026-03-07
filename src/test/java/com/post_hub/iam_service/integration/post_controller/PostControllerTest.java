551e19e95de3802dbe79a735d69d6fbdef340a65
alex20x20 <alex20x20@gmail.com>
2026-03-07 21:21:28 +0300

    post new test

--- src/test/java/com/post_hub/iam_service/integration/post_controller/PostControllerTest.java
+++ src/test/java/com/post_hub/iam_service/integration/post_controller/PostControllerTest.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.post_hub.iam_service.IamServiceApplication;
 import com.post_hub.iam_service.model.entity.User;
 import com.post_hub.iam_service.model.exception.InvalidDataException;
+import com.post_hub.iam_service.model.request.post.NewPostRequest;
 import com.post_hub.iam_service.repository.UserRepository;
 import com.post_hub.iam_service.security.JwtTokenProvider;
 import lombok.Setter;
 import org.hibernate.Hibernate;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -16,8 +18,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -47,5 +53,52 @@ public class PostControllerTest {
 
         Hibernate.initialize(user.getRoles());
         this.currentJwt = "Bearer " + jwtTokenProvider.generateToken(user);
+    }
+
+    @Test
+    void getPosts_OK_200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/posts/all")
+                        .header(HttpHeaders.AUTHORIZATION, currentJwt)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @Transactional
+    void createPost_OK_200() throws Exception {
+        NewPostRequest request = new NewPostRequest("Simple Title", "Simple content", 50);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/posts/create")
+                        .header(HttpHeaders.AUTHORIZATION, currentJwt)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @Transactional
+    void updatePost_OK_200() throws Exception {
+        NewPostRequest request = new NewPostRequest("Updated Title", "Updated content", 50);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/posts/1")
+                        .header(HttpHeaders.AUTHORIZATION, currentJwt)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @Transactional
+    void deletePost_OK_200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/posts/1")
+                        .header(HttpHeaders.AUTHORIZATION, currentJwt)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
